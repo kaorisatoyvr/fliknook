9ef4fbfdd5fc0ab4081853cd2dbf7377afc40829
KhushiMangla <Khushimangla99@gmail.com>
2023-09-09 14:30:05 -0700

    added the styling for mobile version

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import error from '../images/error.png';
 import { NavLink } from 'react-router-dom';
-
+import useMediaQuery from '../hooks/useMediaQuery';
 
 
 
 function Error() {
+    const isDesktop = useMediaQuery('(min-width: 1024px)');
     return (
-     <div class="error__container">
-        <img className="error__img" src={error} alt="error" />
-        <NavLink to= "/">
-        <button className='error__btn'> Go back </button>
-        </NavLink>
+        <div class="error__container">
+            <img className="error__img" src={error} alt="error" />
+            <NavLink to="/">
+                {isDesktop ? (
+                    <button className='error__btn-desktop'> Go back </button>
+                ) : (
+                    <button className='error__btn-mobile'> Go back </button>
+                )
+                }
+            </NavLink>
 
-       
-     </div>
+
+        </div>
     )
 
 }
