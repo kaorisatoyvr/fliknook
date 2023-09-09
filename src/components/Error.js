f92d4a023e1edf91c4160e97012238e99ea03e22
KhushiMangla <Khushimangla99@gmail.com>
2023-09-09 14:09:13 -0700

    added a 404 png image

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import error from '../images/error.png';
+
+
+
 
 function Error() {
     return (
-     <p>Error</p>
+     <div class="error__container">
+        <img className="error__img" src={error} alt="error" />
+        <button className='error__btn'> Go back </button>
+
+       
+     </div>
     )
 
 }
