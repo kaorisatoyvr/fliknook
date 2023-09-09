5dc5e499d48ab53a857cee758fe325d75ad29eb4
KhushiMangla <Khushimangla99@gmail.com>
2023-09-09 14:12:53 -0700

    added a navlink on button so that it directs to home page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import error from '../images/error.png';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -8,7 +9,9 @@ function Error() {
     return (
      <div class="error__container">
         <img className="error__img" src={error} alt="error" />
+        <NavLink to= "/">
         <button className='error__btn'> Go back </button>
+        </NavLink>
 
        
      </div>
