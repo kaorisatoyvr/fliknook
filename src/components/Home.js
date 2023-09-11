171923d90c5ab73bff328f98e5f60185a375fd45
KhushiMangla <Khushimangla99@gmail.com>
2023-09-11 11:50:44 -0700

    added the optional chaining

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ function Home() {
             };
             const response = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", options);
             const data = await response.json();
-            let twelve = data.results.slice(0, 12);
+            let twelve = data?.results.slice(0, 12);
             setMovieList(twelve);
         };
         getMovieList();
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -23,6 +23,7 @@ const Movie = () => {
         };
         const response = await fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?language=en-US&page=1`, options);
         const data = await response.json();
+        console.log(data.results);
         let twelve = data.results.slice(0, 12);
         setMovieType(twelve);
     };
