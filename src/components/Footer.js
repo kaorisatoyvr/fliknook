ce271e8588d416dff2fc7ef5b32fc582681b9fd1
Kaori Sato <ksato9@my.bcit.ca>
2023-10-08 23:34:24 -0700

    adjust footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,9 @@ import favoriteIcon from '../images/heart-red.png';
 
 function Footer() {
     return (
-    <footer>
+    <footer className="footer">
     <a href="/"><img src={logo} alt="logo of FlikNook" /></a>
         <div>
-            {/* <p>&copy; Khushi & Kaori 2023</p> */}
             <p>Hand coded with <span className="footer-heart"><img  src={favoriteIcon} alt="Heart" /></span> | &copy; Khushi & Kaori 2023</p>
         </div>
     </footer>
